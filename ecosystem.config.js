3f498ddb0bb4c8f37a8fbead453a27020d1abb5e
Hamed ElahiFar <hamed.elahifar@gmail.com>
2026-02-24 14:17:37 +0330

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 module.exports = {
   apps: [
     {
-      name: 'Tickado V2',
+      name: 'tickado-v2',
       script: 'dist/main.js',
       interpreter: 'node',
       env: {
         ...process.env,
         NODE_ENV: process.env.NODE_ENV || 'production',
       },
-      instances: 'max',
-      exec_mode: 'cluster', // 'fork' or 'cluster'
+      instances: 1, //'max',
+      exec_mode: 'fork', // 'fork' or 'cluster'
       autorestart: true,
       watch: true,
       max_memory_restart: '512M',
