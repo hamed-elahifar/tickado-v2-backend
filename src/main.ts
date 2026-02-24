c149e82aa81469c16f01998e1b9c762728f088de
Hamed ElahiFar <hamed.elahifar@gmail.com>
2026-02-24 13:39:55 +0330

    add besmellah

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+// =============== ﷽ ===============
+
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
